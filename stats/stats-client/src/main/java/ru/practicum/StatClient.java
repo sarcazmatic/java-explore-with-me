daeffc767548fec664dcc75da4be8ca8cc24cc49
sarcazmatic <sarcazmatic@yandex.ru>
2023-05-25 23:53:35 +0300

    style: Diploma-stage 1 v0.0.2

--- stats/stats-client/src/main/java/ru/practicum/StatClient.java
+++ stats/stats-client/src/main/java/ru/practicum/StatClient.java
@@ -6,5 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StatClient {
     public static void main(String[] args) {
-        SpringApplication.run(StatClient.class, args);    }
+        SpringApplication.run(StatClient.class, args);
+    }
 }
